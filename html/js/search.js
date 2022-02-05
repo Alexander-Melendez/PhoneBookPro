fc3cb097322ace854588bc77c398534b499af79f
xTye <77994279+xTye@users.noreply.github.com>
2022-02-05 11:48:29 -0500

    Search Page Init

---
+++ html/js/search.js
@@ -0,0 +1,79 @@
+const urlBase = 'http://phonebookpro.me/API';
+const extension = 'php';
+
+let firstName = "";
+let lastName = "";
+
+function addColor()
+{
+	let newColor = document.getElementById("colorText").value;
+	document.getElementById("colorAddResult").innerHTML = "";
+
+	let tmp = {color:newColor,userId,userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/AddColor.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("colorAddResult").innerHTML = "Color has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorAddResult").innerHTML = err.message;
+	}
+	
+}
+
+function searchUser()
+{
+	let search = document.getElementById("send").value;
+	document.getElementById("result").innerHTML = "";
+	
+	let list = "";
+
+	let tmp = {search:search,userId:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/PhonebookSearch.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				let json = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<json.results.length; i++ )
+				{
+					list += json.results[i];
+					if( i < json.results.length - 1 )
+					{
+						list += "<br />\r\n";
+					}
+				}
+				
+				document.getElementById("result").innerHTML = list;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("colorSearchResult").innerHTML = err.message;
+	}
+}
